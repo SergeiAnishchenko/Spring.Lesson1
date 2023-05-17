29b10a79eaa770f83e135a0ec616d277d3e6c3ca
SergeiAnishchenko <79313219184@yandex.ru>
2023-05-17 23:10:07 +0300

    Домашка по Spring.Lesson1.

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,14 @@
-public class Car {
+import javax.annotation.PostConstruct;
+
+public class Car extends Transport{
+
+    public Car(String transportType) {
+        super(transportType);
+    }
+
+    @PostConstruct
+    private void init(){
+        Car car = new Car("Автомобиль");
+        System.out.println(car.getTransportType() + " готов к работе.");
+    }
 }
--- src/main/java/SpringApp.java
+++ src/main/java/SpringApp.java
@@ -1,5 +1,18 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringApp {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
+        Driver driver1 = context.getBean("driver1", Driver.class);
+        Driver driver2 = context.getBean("driver2", Driver.class);
+        Driver driver3 = context.getBean("driver3", Driver.class);
+
+        context.close();
+
+        System.out.println(driver1);
+        System.out.println(driver2);
+        System.out.println(driver3);
     }
 }
